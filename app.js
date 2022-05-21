a95fc739fab82660009f4a2cb2dc9f186a5d9088
Ammarkhan96 <102145723+Ammarkhan96@users.noreply.github.com>
2022-05-21 12:38:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function bulbOn() {
+    var bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src", "images/bulb on.png")
+}
+
+function bulbOff() {
+    var bulbobj = document.querySelector(".bulboff")
+    bulbobj.setAttribute("src", "images/bulb off.png")
+}
+
